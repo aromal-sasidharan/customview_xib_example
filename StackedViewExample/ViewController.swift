3ad51bc1dd6871066880b1a41bbeb07abd8fc5ee
Aromal <aromalsasidharan@gmail.com>
2018-09-26 22:56:22 +0800

    Initial Commit

---
+++ StackedViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackedViewExample
+//
+//  Created by Aromal Sasidharan on 9/26/18.
+//  Copyright © 2018 Aromal Sasidharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
